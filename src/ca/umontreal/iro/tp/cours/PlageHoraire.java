84e526e24cc9da7957222dfce49ca6adba888485
ilya2s <ilyass.elouazzani97@outlook.fr>
2022-07-01 03:32:56 -0400

    modified toString to return french day names

--- src/ca/umontreal/iro/tp/cours/PlageHoraire.java
+++ src/ca/umontreal/iro/tp/cours/PlageHoraire.java
@@ -2,12 +2,18 @@ package ca.umontreal.iro.tp.cours;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public record PlageHoraire(LocalDate jour, LocalTime debut, LocalTime fin) {
 
     @Override
     public String toString() {
-        return jour.getDayOfWeek().toString() + " (" + debut + " - " + fin + ")";
+        return jour
+                .getDayOfWeek()
+                .getDisplayName(TextStyle.FULL, Locale.CANADA_FRENCH)
+                .toUpperCase(Locale.ROOT)
+                + " (" + debut + " - " + fin + ")";
     }
 
 }
